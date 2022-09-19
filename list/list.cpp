2eac46cf9a9e79d7badc003a4c1d4e2ad598d006
Windfall-Online <109807780+Windfall-Online@users.noreply.github.com>
2022-09-20 01:03:38 +0900

    Create list.cpp

---
+++ list/list.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <list>
+#include <vector>
+using namespace std;
+
+int main()
+{
+
+	list<int> li;
+
+	list<int>::iterator eraseit;
+
+	for (int i = 0; i < 10; ++i)
+	{
+
+	}
+
+}
+
